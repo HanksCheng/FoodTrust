bc296da8bd4a891c596a760c21b98d4d5b0ef7df
HanksCheng <43198381+HanksCheng@users.noreply.github.com>
2021-10-11 16:39:53 +0800

    Add files via upload

---
+++ food_BlockChain_test/www/js/choose_store_conn.js
@@ -0,0 +1,25 @@
+<<<<<<< HEAD
+
+console.log(localStorage.address);
+function check(){
+    if(localStorage.address==undefined){
+        window.location.href='firstone-login.html';
+    }
+    else{
+        window.location.href='store-login.html';
+    }
+};
+    
+=======
+
+console.log(localStorage.address);
+function check(){
+    if(localStorage.address==undefined){
+        window.location.href='firstone-login.html';
+    }
+    else{
+        window.location.href='store-login.html';
+    }
+};
+    
+>>>>>>> 45f6acb503f5d4b0fcc616f5d5329af12d329364
